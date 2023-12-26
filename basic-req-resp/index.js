f5621b135d042341cd787af1302955fd677163c1
jayesh315thinkitive <jayesh.thinkitive@gmail.com>
2023-12-26 16:01:53 +0530

    feat : add response header code

--- basic-req-resp/index.js
+++ basic-req-resp/index.js
@@ -9,8 +9,9 @@ const data = [
 ]
 
 http.createServer((req,res) =>{
-    console.log(req);
+    // console.log(req);
     // res.write("Hello, welcome to the Home page.")
+    res.writeHead(200,{'Content-Type' : 'application/json','x-custome-header' : 'jayesh gangurde'})
     res.write(JSON.stringify(data))
     res.end();
 }).listen(5500,()=>{
