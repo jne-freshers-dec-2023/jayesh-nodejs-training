adc71d6e52873d2acdaa9c5c0790b56675a40efa
jayesh315thinkitive <jayesh.thinkitive@gmail.com>
2023-12-28 16:45:14 +0530

    Feat : add using express router code

---
+++ basic-express/using-express-router/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.listen(5100, () => {
+  console.log("server is listening on port : 5100");
+});
