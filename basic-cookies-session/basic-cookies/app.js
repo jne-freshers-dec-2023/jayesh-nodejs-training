1caeeb505603212250f1966c7003a6b2371ae0d2
jayesh315thinkitive <jayesh.thinkitive@gmail.com>
2024-01-08 19:03:58 +0530

    fix: reformat codebase

--- basic-cookies-session/basic-cookies/app.js
+++ basic-cookies-session/basic-cookies/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
@@ -11,25 +11,25 @@ app.get("/", (req, res) => {
 
 app.get("/set-cookies", (req, res) => {
   // res.setHeader("set-cookie", "isLoggedIn=true");
-  res.cookie('isLoggedIn',true)
-  res.cookie('userId',12315 ,{
+  res.cookie("isLoggedIn", true);
+  res.cookie("userId", 12315, {
     // maxAge : 5000,
-    expires : new Date('6 Jan 2024'),
-    httpOnly : true,
-    secure : true
-  })
-  
+    expires: new Date("6 Jan 2024"),
+    httpOnly: true,
+    secure: true,
+  });
+
   res.send("<h1>Cookies is set.</h1>");
 });
 
 app.get("/get-cookies", (req, res) => {
   console.log(req.cookies);
-  res.send(req.cookies)
+  res.send(req.cookies);
 });
 
 app.get("/del-cookies", (req, res) => {
-  res.clearCookie('userId')
-  res.send(req.cookies)
+  res.clearCookie("userId");
+  res.send(req.cookies);
 });
 
 app.listen(3000, () => {
