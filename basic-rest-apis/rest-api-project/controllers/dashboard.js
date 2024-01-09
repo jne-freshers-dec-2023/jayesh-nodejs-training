a8a2e86b3980998d2b0e677be74c430f827246b1
jayesh315thinkitive <jayesh.thinkitive@gmail.com>
2024-01-09 13:10:39 +0530

    fix : add try catch block to dashboard controller

--- basic-rest-apis/rest-api-project/controllers/dashboard.js
+++ basic-rest-apis/rest-api-project/controllers/dashboard.js
@@ -1,30 +1,27 @@
+const express = require("express");
+const User = require("../models/user");
 
-const express = require('express')
-const User = require("../models/user")
+exports.userInfo = async (req, res, next) => {
+  try {
+    const users = await User.find();
 
+    const userInfo = users.map((user) => {
+      let userData = {};
+      (userData.Name = user.name), (userData.TotalPost = user.posts.length);
+      console.log(userData);
 
-exports.userInfo = (req,res,next) =>{
+      return userData;
+    });
+    console.log("user Info ", userInfo);
 
- User.find()
-    .then((users) =>{
-       return users.map((user) => {
-            let userData = {}
-           userData.Name =  user.name,
-           userData.TotalPost = user.posts.length
-           console.log(userData);
-
-           return userData;
-        })
-    }).then(userInfo => {
-        console.log("user Info ",userInfo);
-        return res.status(200).json({
-            message : "User info fetched successfully.",
-            userDetails : userInfo
-        })
-    }).catch((err) => {
-        if (!err.statusCode) {
-          err.statusCode = 500;
-        }
-        next(err);
-      });
-}
+    return res.status(200).json({
+      message: "User info fetched successfully.",
+      userDetails: userInfo,
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
